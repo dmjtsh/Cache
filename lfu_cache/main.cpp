6313543d934e7fe0b7180070d55f5da53a11769e
dmjtsh <cyber.pelmen@yandex.ru>
2024-09-25 19:49:11 +0300

    added Unit tests

--- lfu_cache/main.cpp
+++ lfu_cache/main.cpp
@@ -7,36 +7,85 @@
 
 #include "lfu_cache.h"
 
-bool TestLFUCache()
+size_t TestLFUCache(std::istream& input)
 {
     size_t cache_hit_count = 0;
 
     int cache_capacity = 0;
-    std::cin >> cache_capacity;
+    input >> cache_capacity;
 
     LFUCache Cache(cache_capacity);
 
     int elems_count = 0;
-    std::cin >> elems_count;
+    input >> elems_count;
 
     int elem_key = 0;
     for (size_t i = 0; i < elems_count; i++)
     {
-        std::cin >> elem_key;
+        input >> elem_key;
         if (Cache.get(elem_key) != -1)
             cache_hit_count++;
         else
             Cache.put(elem_key, FICT_ELEM_VALUE);
     }
 
-    std::cout << cache_hit_count << std::endl;
-
-    return true;
+    return cache_hit_count;
 }
 
-int main(int argc, const char** argv)
+std::stringstream OpenFileAndReturnStrStream(std::string file_name)
 {
-    TestLFUCache();
+    std::ifstream file(file_name);
 
-    return 0;
+    std::stringstream empty_ss("");
+
+    if (!file) {
+        std::cerr << "Could not open a test file!" << std::endl;
+        return empty_ss;
+    }
+
+    std::string line = "";
+    // if string stream not empty
+    if (getline(file, line))
+        return std::stringstream(line);
+
+    return empty_ss;
+}
+
+#include <gtest/gtest.h>
+
+TEST(TestLFUCacheCorrectness, Subtest_1) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test1.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestLFUCache(ss) == 3);
+}
+
+TEST(TestLFUCacheCorrectness, Subtest_2) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test2.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestLFUCache(ss) == 6);
+}
+
+TEST(TestLFUCacheCorrectness, Subtest_3) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test3.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestLFUCache(ss) == 694);
+}
+
+TEST(TestLFUCacheCorrectness, Subtest_4) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test4.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestLFUCache(ss) == 665463);
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << TestLFUCache(std::cin) << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
--- pca_cache/main.cpp
+++ pca_cache/main.cpp
@@ -7,22 +7,21 @@
 
 #include "pca_cache.h"
 
-bool TestPCACache()
+size_t TestPCACache(std::istream& input)
 {
-
     int cache_capacity = 0;
-    std::cin >> cache_capacity;
+    input >> cache_capacity;
 
     PCACache PCACache(cache_capacity);
 
     int elems_count = 0;
-    std::cin >> elems_count;
+    input >> elems_count;
 
     int* elems_array = new int[elems_count];
     int elems_key = 0;
     for (size_t i = 0; i < elems_count; i++)
     {
-        std::cin >> elems_key;
+        input >> elems_key;
         *(elems_array+i) = elems_key;
     }
 
@@ -30,14 +29,64 @@ bool TestPCACache()
 
     delete[] elems_array;
 
-    std::cout << cache_hit_count << std::endl;
-
-    return true;
+    return cache_hit_count;
 }
 
-int main(int argc, const char** argv)
-{
-    TestPCACache();
 
-    return 0;
+std::stringstream OpenFileAndReturnStrStream(std::string file_name)
+{
+    std::ifstream file(file_name);
+
+    std::stringstream empty_ss("");
+
+    if (!file) {
+        std::cerr << "Could not open a test file!" << std::endl;
+        return empty_ss;
+    }
+
+    std::string line = "";
+    // if string stream not empty
+    if (getline(file, line))
+        return std::stringstream(line);
+
+    return empty_ss;
+}
+
+#include <gtest/gtest.h>
+
+TEST(TestPCACacheCorrectness, Subtest_1) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test1.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestPCACache(ss) == 3);
+}
+
+TEST(TestPCACacheCorrectness, Subtest_2) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test2.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestPCACache(ss) == 7);
+}
+
+TEST(TestPCACacheCorrectness, Subtest_3) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test3.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestPCACache(ss) == 857);
+}
+
+TEST(TestPCACacheCorrectness, Subtest_4) {
+    std::stringstream ss = OpenFileAndReturnStrStream("../tests/test4.txt");
+    ASSERT_FALSE(ss.str().empty());
+
+    ASSERT_TRUE(TestPCACache(ss) == 894836);
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << TestPCACache(std::cin) << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
